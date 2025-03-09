ba4854cfbe9179c13bcd5e6a8b1c3b3c2baf55e4
sophiaBuss0410 <76047079+sophiaBuss0410@users.noreply.github.com>
2025-03-09 18:14:18 +0200

    Update the readme.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ import branca
 import base64
 
 from utils.map_utils import create_folium_map
+from utils.map_risk import create_highrisk_map
 from streamlit_folium import folium_static
 
 import plotly.graph_objects as go
@@ -40,15 +41,17 @@ make_map_responsive= """
 """
 st.markdown(make_map_responsive, unsafe_allow_html=True)
 
-tab1, tab2, tab3 = st.tabs(["Monitoring", "Prediction", "Impact"])
+# Sidebar for navigation
+st.sidebar.title("")
+page = st.sidebar.radio("Go to", ["Monitoring", "Prediction", "Impact"])
 
-with tab1:
 
+if page == "Monitoring":
     # Title of the dashboard
     st.title("Monitoring Drug Usage in Finland")
 
     # Creating a grid with three columns for the top section
-    col1, col2, col3 = st.columns([1.5, 1.5, 0.5])
+    col1, col2 = st.columns([1, 1])
 
     # Chart 1: Number of deaths
     with col1:
@@ -67,6 +70,7 @@ with tab1:
                            template="plotly_white", showlegend=True)
         st.plotly_chart(fig1, use_container_width=True)
 
+    with col2:
         st.subheader("Drug Price Trends")
         data = pd.read_csv("data/clean/Retail_drug_prices.csv")
         data["Year"] = data["Year"].astype(int)
@@ -92,14 +96,67 @@ with tab1:
                            template="plotly_white", showlegend=True)
         st.plotly_chart(fig2, use_container_width=True)
 
-    # Center column with map
-    with col2:
+
+    with st.container():
+        st.subheader("Trends by regions")
+        
+        kpi_options = ["arrests", "offences", "rehab", "clinic"]
+        selected_kpis = st.multiselect("Select KPI(s):", options=kpi_options, default=["arrests"])
+
         # # Mode selection
-        # mode = st.radio("Select Mode", options=["View in Time Series", "View in Static"])
+        # mode = st.radio("Select Mode", options=["View in dynamic mode", "Examine in static mode"])
 
-        # if mode == "View in Time Series":
-        #     st.subheader("Drug-related arrests by regions")
+        # if mode == "View in dynamic mode":
+        cont_col1, cont_col2 = st.columns(2)
 
+        with cont_col1:
+            # Load the GIF
+            gif_path = "data/drug_usage_maps_2000_2023.gif"
+            with open(gif_path, 'rb') as file:
+                contents = file.read()
+            data_url = base64.b64encode(contents).decode('utf-8-sig')
+            st.markdown(f'<img src="data:image/gif;base64,{data_url}">', unsafe_allow_html=True)
+
+        with cont_col2:
+            ## line plots 
+
+            df = pd.read_csv("data/merged_TSA.csv")
+            
+            # Create a selectbox for region filtering.
+            unique_regions = sorted(df["region"].unique())
+            region_selected = st.selectbox("Select Region:", unique_regions)
+
+            # Filter the DataFrame based on the selected region.
+            filtered_df = df[df["region"] == region_selected]
+
+            # Create a Plotly line chart.
+            fig = go.Figure()
+
+            # Optional: Define colors for each KPI for consistency.
+            kpi_colors = {"arrests": "blue", "offences": "red", "rehab": "green", "clinic": "orange"}
+
+            # Add a trace for each selected KPI.
+            for kpi in selected_kpis:
+                fig.add_trace(go.Scatter(
+                    x=filtered_df['year'],
+                    y=filtered_df[kpi],
+                    mode='lines+markers',
+                    name=kpi.capitalize(),
+                    line=dict(color=kpi_colors.get(kpi, "black"))
+                ))
+
+            # Update layout for clarity.
+            fig.update_layout(
+                title=f"KPI Trends for {region_selected}",
+                xaxis_title="Year",
+                yaxis_title="Count",
+                template="plotly_white"
+            )
+
+            st.plotly_chart(fig, use_container_width=True)
+            
+
+        # elif mode == "Examine in static mode":
         #     df = pd.read_csv("./data/clean/Reported_drug_usage_by_regions.csv")
 
         #     # Select year
@@ -108,52 +165,9 @@ with tab1:
         #     # Create and display the map
         #     m = create_folium_map(year)
         #     folium_static(m, width=800, height=900)
-            
-        # elif mode == "View in Static":
-        st.subheader("Drug-related arrests by regions")
-
-        # Load the GIF
-        gif_path = "data/drug_usage_maps_2000_2023.gif"
-        with open(gif_path, 'rb') as file:
-            contents = file.read()
-        data_url = base64.b64encode(contents).decode('utf-8-sig')
-        st.markdown(f'<img src="data:image/gif;base64,{data_url}">', unsafe_allow_html=True)
 
 
-    # Metrics
-    with col3:
-        # arrests
-        df = pd.read_csv("./data/clean/Reported_drug_usage_by_regions.csv")
-        df_death = pd.read_csv("./data/clean/Drug_related_deaths.csv")
-
-        year_list = list(range(2007, 2024))
-        year = st.selectbox("Select Year", options=year_list, index=16)
-
-        current_year_value = df[df["year"] == year]['KOKO MAA'].values[0]
-        year_before_value = df[df["year"] == year-1]['KOKO MAA'].values[0]
-
-        increase_rate = ((current_year_value - year_before_value) / year_before_value) * 100
-
-        st.metric(f"Drug related **arrests** change (%) in {year}", value=np.round(increase_rate, decimals=2), border=True)
-
-        # deaths
-        df_death.loc['Total'] = df_death.sum(numeric_only=True)
-
-        current_year_value = df_death.loc['Total', str(year)]
-        year_before_value = df_death.loc['Total', str(year-1)]
-
-        increase_rate = ((current_year_value - year_before_value) / year_before_value) * 100
-
-        st.metric(f"Drug related **deaths** change (%) in {year}", value=np.round(increase_rate, decimals=2), border=True)
-
-        # regions
-        max_arrests_region = df[df["year"] == year].drop(columns=["year", "KOKO MAA"]).idxmax(axis=1).values[0]
-        max_arrests_value = df[df["year"] == year][max_arrests_region].values[0]
-
-        st.metric(f"Region with most arrests in {year}", value=max_arrests_region, help=str(max_arrests_value), border=True)
-
-with tab2:
-
+elif page == "Prediction":
     # Load datasets
     offences_df = pd.read_csv("data/clean/offences_forecast.csv", parse_dates=['year'])
     arrests_df = pd.read_csv("data/clean/arrests_forecast.csv", parse_dates=['year'])
@@ -164,7 +178,7 @@ with tab2:
     st.title("Future Trends in Illegal Drug-Related KPIs in Finland")
 
     # Create layout with three columns: Left (2), Spacer (0.05), Right (2)
-    col1, col_space, col2 = st.columns([2, 0.05, 2])
+    col1, col2 = st.columns([2, 2])
 
     ### ---- LEFT SIDE: Offences & Arrests ---- ###
     with col1:
@@ -215,33 +229,7 @@ with tab2:
         st.subheader("Forecast Data Table")
         displayed_df = filtered_df[['year', 'region', 'type', kpi_column, lower_col, upper_col]].sort_values(by='year')
         st.dataframe(displayed_df.style.format({"offences": "{:.2f}", "offences_lower":"{:.2f}", "offences_upper":"{:.2f}",
-                                                "arrests": "{:.2f}", "arrests_lower":"{:.2f}", "arrests_upper":"{:.2f}"}))
-
-        # Divider for better spacing
-        st.divider()
-
-        # Identify Regions with High Increase in 2025
-        def find_high_increase_regions(df, kpi_column):
-            """Finds regions where predictions in 2025 are higher than actuals in 2024 and sorts them by increase."""
-            actuals_2024 = df[(df['year'].dt.year == 2023) & (df['type'] == 'Actual')].set_index('region')[kpi_column]
-            predictions_2025 = df[(df['year'].dt.year == 2025) & (df['type'] == 'Prediction')].set_index('region')[kpi_column]
-
-            increase_df = (predictions_2025 - actuals_2024).dropna()
-            high_regions = increase_df[increase_df > 0].sort_values(ascending=False).reset_index()
-        
-            return high_regions
-
-        # Get high increase regions for offences and arrests
-        high_offences_regions = find_high_increase_regions(offences_df, 'offences')
-        high_arrests_regions = find_high_increase_regions(arrests_df, 'arrests')
-
-        # Display high increase regions
-        st.subheader("Regions with High Increase in Offences (2025 vs 2024)")
-        st.write(high_offences_regions.style.format({"offences": "{:.2f}"}))
-
-        st.subheader("Regions with High Increase in Arrests (2025 vs 2024)")
-        st.write(high_arrests_regions.style.format({"arrests": "{:.2f}"}))
-
+                                                "arrests": "{:.2f}", "arrests_lower":"{:.2f}", "arrests_upper":"{:.2f}"}), height=200)
 
     ### ---- RIGHT SIDE: Deaths Forecast ---- ###
     with col2:
@@ -296,16 +284,46 @@ with tab2:
 
         st.plotly_chart(fig_pie, use_container_width=True)
 
+    with st.container():
+        cont_col1, cont_col2 = st.columns(2)
 
-with tab3:
+        with cont_col1:
+            # Identify Regions with High Increase in 2025
+            def find_high_increase_regions(df, kpi_column):
+                """Finds regions where predictions in 2025 are higher than actuals in 2024 and sorts them by increase."""
+                actuals_2024 = df[(df['year'].dt.year == 2023) & (df['type'] == 'Actual')].set_index('region')[kpi_column]
+                predictions_2025 = df[(df['year'].dt.year == 2025) & (df['type'] == 'Prediction')].set_index('region')[kpi_column]
 
+                increase_df = (predictions_2025 - actuals_2024).dropna()
+                high_regions = increase_df[increase_df > 0].sort_values(ascending=False).reset_index()
+            
+                return high_regions
+
+            # Get high increase regions for offences and arrests
+            high_offences_regions = find_high_increase_regions(offences_df, 'offences')
+            high_arrests_regions = find_high_increase_regions(arrests_df, 'arrests')
+
+            # Display high increase regions
+            st.subheader("Regions with High Increase in Offences (2025 vs 2024)")
+            st.write(high_offences_regions.style.format({"offences": "{:.2f}"}))
+
+            st.subheader("Regions with High Increase in Arrests (2025 vs 2024)")
+            st.write(high_arrests_regions.style.format({"arrests": "{:.2f}"}))
+
+        with cont_col2:
+            st.subheader("High risk regions (2025)")
+
+            m = create_highrisk_map()
+            folium_static(m, width=800, height=900)
+
+elif page == "Impact":
     # review the content
     st.title("Our Initiative of Monitoring and Prediction of drug usage in Finland")
 
     st.header("Who is it for?")
     st.write("""
-    This dashboard is designed for policymakers, healthcare professionals, and researchers who are involved in monitoring and addressing drug usage trends in Finland.\n
-    It provides valuable insights into drug-related deaths, price trends, and regional usage patterns.
+    This dashboard is designed for key stakeholders responsible for addressing and mitigating drug usage in Finland.\n
+    It provides critical insights into drug-related deaths, offenses, price trends, regional usage patterns, and rehabilitation needs.
     """)
 
     st.header("Who benefits from the dashboard?")
